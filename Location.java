0ece96da428badedcb0f9c0d56ba6dc9c875d99a
iQuixilty <76968084+iQuixilty@users.noreply.github.com>
2022-01-28 18:24:26 -0800

    Add files via upload

--- Location.java
+++ Location.java
@@ -1,8 +1,28 @@
+import java.util.Objects;
+
 public class Location {
     int x, y;
 
     public Location(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Location location = (Location) o;
+        return x == location.x && y == location.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return x + " " + y;
     }
 }
